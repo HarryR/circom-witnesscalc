151411111af4ac7660a1c6bbc281c949ba78dab6
Oleg Lomaka <oleg.lomaka@gmail.com>
2025-05-06 11:23:46 -0400

    cvm_compile: accept cmd args

--- src/bin/cvm-compile.rs
+++ src/bin/cvm-compile.rs
@@ -1,10 +1,61 @@
-
-use circom_witnesscalc::vm::ComponentTmpl;
+use std::env;
 use circom_witnesscalc::ast::Literal;
 
+struct Args {
+    cvm_file: String,
+    output_file: String,
+}
+
+
+fn parse_args() -> Args {
+    let mut cvm_file: Option<String> = None;
+    let mut output_file: Option<String> = None;
+
+    let args: Vec<String> = env::args().collect();
+
+    let usage = |err_msg: &str| {
+        if !err_msg.is_empty() {
+            eprintln!("ERROR:");
+            eprintln!("    {}", err_msg);
+            eprintln!();
+        }
+        eprintln!("USAGE:");
+        eprintln!("    {} <cvm_file> <output_path> [OPTIONS]", args[0]);
+        eprintln!();
+        eprintln!("ARGUMENTS:");
+        eprintln!("    <cvm_file>    Path to the CVM file with compiled circuit");
+        eprintln!("    <output_path> File where the witness will be saved");
+        eprintln!();
+        eprintln!("OPTIONS:");
+        eprintln!("    -h | --help                Display this help message");
+        let exit_code = if !err_msg.is_empty() { 1i32 } else { 0i32 };
+        std::process::exit(exit_code);
+    };
+
+    let mut i = 1;
+    while i < args.len() {
+        if args[i] == "--help" || args[i] == "-h" {
+            usage("");
+        } else if args[i].starts_with("-") {
+            usage(format!("Unknown option: {}", args[i]).as_str());
+        } else if cvm_file.is_none() {
+            cvm_file = Some(args[i].clone());
+        } else if output_file.is_none() {
+            output_file = Some(args[i].clone());
+        }
+        i += 1;
+    }
+    Args {
+        cvm_file: cvm_file.unwrap_or_else(|| { usage("missing CVM file"); String::new() }),
+        output_file: output_file.unwrap_or_else(|| { usage("missing output file"); String::new() }),
+    }
+}
+
 fn main() {
+    let args = parse_args();
+
     let _x = Literal::I64(123);
-    println!("OK");
+    println!("OK {}", args.cvm_file);
 }
 
 #[cfg(test)]
