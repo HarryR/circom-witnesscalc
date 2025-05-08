fe6097079409c0ed8a9968c655b0e27e91c88712
Oleg Lomaka <oleg.lomaka@gmail.com>
2025-05-08 05:12:17 -0400

    Implement StoreSignal vm2 operation

--- src/vm2.rs
+++ src/vm2.rs
@@ -7,6 +7,9 @@ pub enum OpCode {
     // Put signals to the stack
     // required stack_i64: signal index
     LoadSignal      = 1,
+    // Store the signal
+    // stack_ff contains the value to store
+    // stack_i64 contains the signal index
     StoreSignal     = 2,
     PushI64         = 3, // Push i64 value to the stack
     PushFf          = 4, // Push i64 value to the stack
@@ -31,9 +34,50 @@ fn read_instruction(code: &[u8], ip: usize) -> OpCode {
 
 pub enum RuntimeError {
     StackUnderflow,
+    StackVariableIsNotSet,
     I32ToUsizeConversion,
     SignalIndexOutOfBounds,
     SignalIsNotSet,
+    SignalIsAlreadySet,
+}
+
+struct VM<T: FieldOps> {
+    stack_ff: Vec<Option<T>>,
+    stack_i64: Vec<Option<i64>>,
+}
+
+impl<T: FieldOps> VM<T> {
+    fn new() -> Self {
+        Self {
+            stack_ff: Vec::new(),
+            stack_i64: Vec::new(),
+        }
+    }
+
+    fn push_ff(&mut self, value: Option<T>) {
+        self.stack_ff.push(value);
+    }
+
+    fn pop_ff(&mut self) -> Result<T, RuntimeError> {
+        self.stack_ff.pop().ok_or(RuntimeError::StackUnderflow)?
+            .ok_or(RuntimeError::StackVariableIsNotSet)
+    }
+
+    fn push_i64(&mut self, value: Option<i64>) {
+        self.stack_i64.push(value);
+    }
+
+    fn pop_i64(&mut self) -> Result<i64, RuntimeError> {
+        self.stack_i64
+            .pop().ok_or(RuntimeError::StackUnderflow)?
+            .ok_or(RuntimeError::StackVariableIsNotSet)
+    }
+
+    fn pop_usize(&mut self) -> Result<usize, RuntimeError> {
+        self.pop_i64()?
+            .try_into()
+            .map_err(|_| RuntimeError::I32ToUsizeConversion)
+    }
 }
 
 pub fn execute<T>(
@@ -43,9 +87,7 @@ where
     T: FieldOps {
 
     let mut ip: usize = 0;
-    let mut stack_ff: Vec<Option<T>> = Vec::new();
-    let mut stack_i64: Vec<Option<i64>> = Vec::new();
-    let mut stack_i64_base: usize = 0;
+    let mut vm = VM::<T>::new();
 
     'label: loop {
         if ip == templates[main_template_id].code.len() {
@@ -58,23 +100,22 @@ where
         match op_code {
             OpCode::NoOp => (),
             OpCode::LoadSignal => {
-                let signal_idx = stack_i64.pop()
-                    // Check that the stack is not empty
-                    .ok_or_else(|| RuntimeError::StackUnderflow)?
-                    // Checkout that the value in the stack is not null
-                    .ok_or_else(|| RuntimeError::StackUnderflow)?;
-
-                let signal_idx: usize = signal_idx.try_into()
-                    .map_err(|_| RuntimeError::I32ToUsizeConversion)?;
-
+                let signal_idx = vm.pop_usize()?;
                 let s = signals.get(signal_idx)
                     .ok_or_else(|| RuntimeError::SignalIndexOutOfBounds)?
                     .ok_or_else(|| RuntimeError::SignalIsNotSet)?;
-                
-                stack_ff.push(Some(s));
+
+                vm.push_ff(Some(s));
             }
             OpCode::StoreSignal => {
-                todo!();
+                let signal_idx = vm.pop_usize()?;
+                if signal_idx >= signals.len() {
+                    return Err(RuntimeError::SignalIndexOutOfBounds);
+                }
+                if signals[signal_idx].is_some() {
+                    return Err(RuntimeError::SignalIsAlreadySet);
+                }
+                signals[signal_idx] = Some(vm.pop_ff()?);
             }
             OpCode::PushI64 => {
                 todo!();
